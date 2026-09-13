1fca201652e3a6a171f9da32eb6ca8072e94c353
QianBenYuan <qianbenyuan@github.com>
2026-09-13 21:33:18 +0800

    chore: 关闭 stylelint 行尾校验
    工作区在 Windows 下是 CRLF，@stylistic/linebreaks 会对每个文件每一行报错。行尾交给 .editorconfig 与编辑器处理，不改动存量文件。

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -4,6 +4,8 @@ export default zin({
 	// @keep-sorted
 	rules: {
 		'@stylistic/indentation': 'tab',
+		// 工作区在 Windows 下是 CRLF，行尾交给 .editorconfig / 编辑器处理，这里不再报错
+		'@stylistic/linebreaks': null,
 		'media-feature-range-notation': 'prefix',
 	},
 })
